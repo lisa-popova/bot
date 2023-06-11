61fb93673bbbc4c039833648903add9c514878e1
Liza <lisa.iv.popova@gmail.com>
2023-06-11 18:49:52 +0300

    стартовый коммит

---
+++ bot.py
@@ -0,0 +1,22 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config_reader import config
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    await message.reply("Привет от pingvi! бот пока в разработке")
+
+
+@dp.message_handler(commands="photo")
+async def picture(message: types.Message):
+    photo = open('photo/genshin.jpg', 'rb')
+    await message.answer_photo(photo=photo, caption='<3')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
