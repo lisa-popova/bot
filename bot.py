2130c5441ed8a97157d134fc93f5f681ae60a3cd
Liza <lisa.iv.popova@gmail.com>
2023-06-17 23:35:25 +0300

    новые команды

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config_reader import config
+from random import randint
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
@@ -9,7 +10,34 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.reply("Привет от pingvi! Бот пока в разработке")
+    kb = [
+        [
+            types.KeyboardButton(text="1 крутка"),
+            types.KeyboardButton(text="10 круток")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+    )
+    await message.answer("Привет, Первопроходец! Хочешь сделать крутку?", reply_markup=keyboard)
+
+
+@dp.message_handler(text="1 крутка")
+async def one_wish(message: types.Message):
+    answer = ''
+    num = randint(0, 1000)
+    if 1 <= num <= 700:
+        answer = 'Оружие 3 звезды'
+    elif 701 <= num <= 900:
+        answer = 'Оружие 4 звезды'
+    elif 901 <= num <= 1000:
+        answer = 'Оружие 5 звёзд'
+    await message.reply(answer)
+
+@dp.message_handler(text="10 круток")
+async def ten_wish(message: types.Message):
+    await message.reply("Выпадает дофига всего!")
 
 @dp.message_handler(commands="info")
 async def start(message: types.Message):
@@ -25,6 +53,10 @@ async def picture(message: types.Message):
     photo = open('photo/eolekino.jpg', 'rb')
     await message.answer_photo(photo=photo, caption='<3')
 
+
+@dp.message_handler(commands="choose_favorite")
+async def choose_favorite(message: types.Message):
+    await message.reply('этот бот для круток')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
