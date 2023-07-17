594f623a79e1a1f387dfa32e233610e1c3c208f9
liza_ <lisa.iv.popova@gmail.com>
2023-07-17 23:05:11 +0300

    Картинка молитв

--- image.py
+++ image.py
@@ -2,39 +2,54 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-#загрузка фонового изображения
-background = Image.open('photo/banner.png')
+import os
+import sqlite3
 
-#загрузка и изменеине размера фото персонажа
-picture = Image.open('photo/hutao.webp')
-width, height = (picture.size)
-newHeight = 500
-newWidth = width * newHeight // height
-size=(newWidth,newHeight)
-pictureResize = picture.resize(size)
-Image.Image.paste(background, pictureResize, (42, -20), mask=pictureResize)
+dir = os.path.abspath(os.curdir)
 
-#наложение элемента
-pictureelem = Image.open('photo/piro.png')
-width, height = (pictureelem.size)
-newHeight = 70
-newWidth = width * newHeight // height
-size=(newWidth,newHeight)
-pictureResize = pictureelem.resize(size)
-Image.Image.paste(background, pictureResize, (6, 205), mask=pictureResize)
+sqlite_connection = sqlite3.connect('genshin.db')
+cursor = sqlite_connection.cursor()
 
-#наложение текста
-draw = ImageDraw.Draw(background)
-font = ImageFont.truetype("font/Default_JP.ttf", 35)
-draw.text((45, 210),"Ху Тао",(255,255,255),font=font)
+cursor.execute("SELECT id, name, image, rarity, element FROM pers order by id")
 
-#наложение звёздочек
-picturestar = Image.open('photo/5star.png')
-width, height = (picturestar.size)
-newHeight = 20
-newWidth = width * newHeight // height
-size=(newWidth,newHeight)
-pictureResize = picturestar.resize(size)
-Image.Image.paste(background, pictureResize, (60, 250), mask=pictureResize)
+for id, name, image, rarity, element in cursor.fetchall():
+    print(name, rarity, image, element)
 
-background.save('photo/result.jpg', quality=95)
+    #загрузка фонового изображения
+    background = Image.open('photo/banner.png').convert('RGBA')
+    bgwidth, bgheight = (background.size)
+
+    #загрузка и изменеине размера фото персонажа
+    picture = Image.open(dir + image).convert('RGBA')
+    width, height = (picture.size)
+    newHeight = 500
+    newWidth = width * newHeight // height
+    size=(newWidth,newHeight)
+    offset = bgwidth - newWidth
+    pictureResize = picture.resize(size)
+    Image.Image.paste(background, pictureResize, (offset, -20), mask=pictureResize)
+
+    #наложение элемента
+    pictureelem = Image.open('element/' + element +'.png').convert("RGBA")
+    width, height = (pictureelem.size)
+    newHeight = 70
+    newWidth = width * newHeight // height
+    size=(newWidth,newHeight)
+    pictureResize = pictureelem.resize(size)
+    Image.Image.paste(background, pictureResize, (6, 205), mask=pictureResize)
+
+    #наложение текста
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype("font/Default_JP.ttf", 35)
+    draw.text((45, 210),name,(255,255,255),font=font)
+
+    #наложение звёздочек
+    picturestar = Image.open('rarity/' + str(rarity) + 'star' + '.png')
+    width, height = (picturestar.size)
+    newHeight = 20
+    newWidth = width * newHeight // height
+    size=(newWidth,newHeight)
+    pictureResize = picturestar.resize(size)
+    Image.Image.paste(background, pictureResize, (60, 250), mask=pictureResize)
+
+    background.save('pers_banner/' + name + '.png', quality=95, format="png")
